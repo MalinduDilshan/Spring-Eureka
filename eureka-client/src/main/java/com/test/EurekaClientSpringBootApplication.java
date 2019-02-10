254a785b74a958ccc0721fb2013728db4060a2a3
MalinduDilshan <malindudilshan389@gmail.com>
2019-02-10 21:48:02 +0530

    Eureka and Client registered with Server

--- eureka-client/src/main/java/com/test/EurekaClientSpringBootApplication.java
+++ eureka-client/src/main/java/com/test/EurekaClientSpringBootApplication.java
@@ -2,7 +2,9 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaClientSpringBootApplication {
     public static void main(String[] args) {
--- eureka-server/src/main/java/com/test/EurekaServerSpringBootApplication.java
+++ eureka-server/src/main/java/com/test/EurekaServerSpringBootApplication.java
@@ -2,7 +2,9 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerSpringBootApplication {
     public static void main(String[] args) {
